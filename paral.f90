df94bb85fa4e67fa5fe8251b6867db4e040cdf9c
Semenishin <33102098+Semenishin@users.noreply.github.com>
2017-11-24 00:38:47 +0300

    FinalVersion
    1)Исправлено для отрицательных матриц
    2)Исправлено оформление
    3)Добавлено адекватное сообщение к коммиту

--- paral.f90
+++ paral.f90
@@ -17,8 +17,6 @@ module Homework
         n = size(A, dim=2) 
         transpos = .FALSE.
 
-
-
         if (m < n) then 
             transpos = .TRUE.   
             B = transpose(A)
@@ -26,85 +24,59 @@ module Homework
             n = size(B, dim=2) 
         else
             B = A     
-            endif
+        endif
 
         allocate(current_column(m))
-   
-        max_sum=-huge(0d0)
+        max_sum=minval(A)
         x1=1
         y1=1
         x2=1
         y2=1
-
-
-
-
-
-   
+        
 !$OMP PARALLEL
-       
-!$OMP single
-         amount_of_threads=omp_get_num_threads()
-           allocate(max_sum_arr(amount_of_threads))
-           allocate(X_1(amount_of_threads))
-            allocate(X_2(amount_of_threads))
-            allocate(Y_1(amount_of_threads))
-            allocate(Y_2(amount_of_threads))
-                max_sum_arr=-huge(0d0)
-                X_1=1
-                X_2=1
-                Y_1=1
-                Y_2=1
-
-
-!$OMP end single
-  !$OMP   DO  SCHEDULE(dynamic) private(L,R,current_column,current_sum,Up,Down,x1,x2,y1,y2,num_thread,max_sum)
-
+    !$OMP single
+            amount_of_threads=omp_get_num_threads()
+             allocate(max_sum_arr(amount_of_threads))
+             allocate(X_1(amount_of_threads))
+             allocate(X_2(amount_of_threads))
+             allocate(Y_1(amount_of_threads))
+             allocate(Y_2(amount_of_threads))
+             max_sum_arr=max_sum
+             X_1=1
+             X_2=1
+             Y_1=1
+             Y_2=1
+    !$OMP end single
+    !$OMP   DO  SCHEDULE(dynamic) private(L,R,current_column,current_sum,Up,Down,x1,x2,y1,y2,num_thread,max_sum)
         do L=1, n
-       
             current_column = B(:, L) 
-              
-             do R=L,n
- 
+            do R=L,n
                 if (R > L) then 
                     current_column = current_column + B(:, R)
                 endif
-                
                 call FindMaxInArray(current_column, current_sum, Up, Down) 
-              
                 if (current_sum > max_sum) then
                     max_sum = current_sum
-                   ! x1 = Up
-                   ! x2 = Down
-                   ! y1 = L
-                   ! y2 = R
-                    
-                     num_thread=omp_get_thread_num()+1
-                    
-                   
+                    num_thread=omp_get_thread_num()+1
                     max_sum_arr(num_thread)=max_sum
                     X_1(num_thread)=Up
                     X_2(num_thread)=Down
                     Y_1(num_thread)=L
                     Y_2(num_thread)=R
-                    
                 endif
-              
- 
-        end do
-       
-   end do     
+              end do
+        end do     
       
-!$OMP END DO  
+    !$OMP END DO  
 !$OMP END PARALLEL
 
         deallocate(current_column)
         max_sum=maxval(max_sum_arr)
         
-        do i=1,amount_of_threads
+        do i=1,16
             if(max_sum_arr(i)==max_sum) then
                  exit
-                 endif
+            endif
         enddo
       
         x1=X_1(i)
@@ -143,28 +115,22 @@ module Homework
             Down = 1
             cur_sum = 0
             minus_pos = 0
-
-
-   
             do i=1, size(a)
                 cur_sum = cur_sum + a(i)
-            if (cur_sum > Sum) then
-                Sum = cur_sum
-                Up = minus_pos + 1
-                Down = i
+                 if (cur_sum > Sum) then
+                     Sum = cur_sum
+                     Up = minus_pos + 1
+                     Down = i
                 endif
          
             if (cur_sum < 0) then
                 cur_sum = 0
                 minus_pos = i
-                endif
+            endif
 
             enddo
-    
-
-        end subroutine FindMaxInArray
-
-
+     end subroutine FindMaxInArray
 end module Homework
+
 
 
